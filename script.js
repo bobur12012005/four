6392cdf3eb6210534190c91596b4cb03805cfcdf
bobur <boburmakhmadkulov05@gmail.com>
2023-10-12 13:43:02 +0500

    second comment

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let password
-let attempts = 0
+let chances = 0
 do {
     password = +prompt("Enter your code:")
-    attempts += 1
-} while (password !== 1111 && attempts < 3) {
+    chances += 1
+} while (password !== 1111 && chances < 3) {
     if (password === 1111) alert("Everything is fine!")
     else alert("You have no more attempt left!")
 }
